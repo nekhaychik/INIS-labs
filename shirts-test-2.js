da7d3cd13d39198bc2477ceb8144a1346f3d70bc
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-02-08 13:03:37 +0300

    add test files

---
+++ shirts-test-2.js
@@ -0,0 +1,27 @@
+var shirts = [{
+    "name": "Beep Boop",
+    "description": "Once upon a time, a mighty guide guarded the intersection of Forbes and Morewood, and would dutifully direct distracted college students when it was safe to cross the street. Its voice was soothing, strong, and steady. Its name was beep boop.",
+    "price": "$19.99",
+    "colors": {
+        "white": {
+            "front": "shirt_images/beepboop-white-front.png",
+            "back": "shirt_images/beepboop-white-back.png"
+        },
+        "blue": {
+            "front": "shirt_images/beepboop-blue-front.png",
+            "back": "shirt_images/beepboop-blue-back.png"
+        },
+        "pink": {
+            "front": "shirt_images/beepboop-pink-front.png",
+            "back": "shirt_images/beepboop-pink-back.png"
+        },
+        "red": {
+            "front": "shirt_images/beepboop-red-front.png",
+            "back": "shirt_images/beepboop-red-back.png"
+        }
+    },
+    "default": {
+        "front": "shirt_images/default-m-front.png",
+        "back": "shirt_images/default-m-back.png"
+    }
+}]
