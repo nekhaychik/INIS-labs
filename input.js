5803d0e6d18e7f7306b0b3994cbb53e5e43bd17a
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-04-02 20:46:32 +0300

    add a dragndrop event handler for target div

--- input.js
+++ input.js
@@ -1,7 +1,42 @@
 let selectedDiv;
 const workspace = document.getElementById("workspace");
+const divs = document.querySelectorAll(".target");
 
-workspace.onclick = function (event) {
+workspace.addEventListener("click", clickHandler);
+
+divs.forEach((div) => {
+  div.onmousedown = function (event) {
+    let temp = 0;
+
+    div.style.position = "absolute";
+    div.style.zIndex = "1000";
+    document.body.append(div);
+
+    let shiftX = event.clientX - div.getBoundingClientRect().left;
+    let shiftY = event.clientY - div.getBoundingClientRect().top;
+
+    function moveAt(pageX, pageY) {
+      div.style.left = pageX - shiftX + "px";
+      div.style.top = pageY - shiftY + "px";
+      temp++;
+    }
+
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+
+    document.addEventListener("mousemove", onMouseMove);
+
+    div.onmouseup = function () {
+      document.removeEventListener("mousemove", onMouseMove);
+      div.onmouseup = null;
+      workspace.append(div);
+      if (temp === 0) clickHandler(event);
+    };
+  };
+});
+
+function clickHandler(event) {
   let target = event.target.closest(".target");
   if (!target) {
     if (selectedDiv) {
@@ -10,7 +45,7 @@ workspace.onclick = function (event) {
     return;
   }
   changeColorToBlue(target);
-};
+}
 
 function changeColorToBlue(div) {
   if (selectedDiv) {
