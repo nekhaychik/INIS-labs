5de9e02274fad25f795101ed38a570119c48a0f2
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-04-02 18:38:00 +0300

    add files for lab 2

---
+++ input.js
@@ -0,0 +1,8 @@
+/*
+* all the code for laboratory work 2 goes into this file.
+You will attach event handlers to the document, workspace, and targets defined in the html file
+to handle mouse, touch and possible other events.
+
+You will certainly need a large number of global variables to keep track of the current modes and states
+of the interaction.
+*/
