5a9b61f2f8aa56ea688175f23076bbc7a84ccffb
nekhaychik <irina.nekhaychik.i@gmail.com>
2022-05-05 00:11:08 +0300

    add touch event listeners

--- input.js
+++ input.js
@@ -2,8 +2,6 @@ const BLUE_BACKGROUND = "selected";
 const YELLOW_BACKGROUND = "dbSelected";
 const TARGET_CLASS = ".target";
 
-const movableTargetElement = null;
-
 let onclickedDiv;
 let dbclickedDiv;
 let currentDiv = {
@@ -11,9 +9,8 @@ let currentDiv = {
   coords: {
     X: null,
     Y: null,
-  }
+  },
 };
-
 
 const workspace = document.getElementById("workspace");
 
@@ -87,7 +84,7 @@ function moveDiv(event) {
   };
 
   document.onkeydown = function (event) {
-    if (event.code === 'Escape' && currentDiv.div) {
+    if (event.code === "Escape" && currentDiv.div) {
       document.removeEventListener("mousemove", onMouseMove);
       currentDiv.div.style.left = currentDiv.coords.X;
       currentDiv.div.style.top = currentDiv.coords.Y;
@@ -142,3 +139,223 @@ function removeCoords() {
   currentDiv.coords.Y = null;
   dbclickedDiv = null;
 }
+
+const targetElements = document.querySelectorAll(TARGET_CLASS);
+
+const INITIAL_TARGET_COLOR = "red";
+const SELECTED_TARGET_COLOR = "blue";
+const MOVABLE_TARGET_COLOR = "yellow";
+
+const EVENT_LISTENER_OPTIONS = { once: true };
+
+let selectedTargetElement;
+let x, y;
+
+let movableTargetElement;
+let moveAt;
+
+let isTouch = false;
+
+let width;
+let height;
+let distance;
+
+function targetMovement(event, targetElement) {
+  updateTargetCoordinates(targetElement);
+  const shiftX = event.clientX - x;
+  const shiftY = event.clientY - y;
+
+  return (pageX, pageY) => {
+    targetElement.style.left = pageX - shiftX + "px";
+    targetElement.style.top = pageY - shiftY + "px";
+  };
+}
+
+function updateTargetCoordinates(targetElement) {
+  const rect = targetElement.getBoundingClientRect();
+  x = rect.left;
+  y = rect.top;
+}
+
+function computeDistance(x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
+
+function onContainerTouchstartListener() {
+  if (movableTargetElement) {
+    workspace.removeEventListener(
+      "touchstart",
+      onContainerTouchstartListener
+    );
+    movableTargetElement.removeEventListener(
+      "touchmove",
+      onTargetTouchmoveListener
+    );
+
+    movableTargetElement.style.left = x + "px";
+    movableTargetElement.style.top = y + "px";
+
+    movableTargetElement = null;
+
+    console.log("touchstart — container");
+  }
+}
+
+function onTargetTouchmoveListener(event) {
+  const touch = event.touches[0];
+  if (!isDoubleClickMode) {
+    if (event.touches.length === 2) {
+      const touch0 = event.touches[0];
+      const touch1 = event.touches[1];
+      if (touch0.target === touch1.target) {
+        const newDistance = computeDistance(
+          touch0.pageX,
+          touch0.pageY,
+          touch1.pageX,
+          touch1.pageY
+        );
+        const dif = newDistance - distance;
+
+        touch0.target.style.width = width + dif / 2 + "px";
+        touch0.target.style.height = height + dif / 2 + "px";
+        touch0.target.style.left = x - dif / 4 + "px";
+        touch0.target.style.top = y - dif / 4 + "px";
+      }
+    } else {
+      onTargetMoveListener(touch);
+    }
+  } else {
+    if (touch.target === selectedTargetElement) {
+      onTargetMoveListener(touch);
+    } else if (touch.target === event.currentTarget) {
+      onTargetMoveListener({
+        target: selectedTargetElement,
+        pageX: touch.pageX,
+        pageY: touch.pageY,
+      });
+    }
+  }
+}
+
+function onTargetTouchstartListener(event) {
+  if (event.touches.length === 2) {
+    workspace.removeEventListener(
+      "touchstart",
+      onTargetTouchstartListener
+    );
+    workspace.removeEventListener(
+      "touchmove",
+      onTargetTouchmoveListener
+    );
+
+    selectedTargetElement.style.left = x + "px";
+    selectedTargetElement.style.top = y + "px";
+    selectedTargetElement.style.backgroundColor = INITIAL_TARGET_COLOR;
+
+    selectedTargetElement = null;
+    isDoubleClickMode = false;
+  }
+}
+
+targetElements.forEach((targetElement) => {
+  targetElement.addEventListener("touchstart", (event) => {
+    event.stopPropagation();
+
+    const startTime = new Date();
+
+    const touch = event.touches[0];
+    if (!isDoubleClickMode) {
+      if (
+        event.target.classList.contains("target") &&
+        movableTargetElement &&
+        event.target !== movableTargetElement
+      ) {
+        return;
+      }
+
+      moveAt = targetMovement(touch, targetElement);
+
+      if (event.touches.length === 2) {
+        const rect = targetElement.getBoundingClientRect();
+        width = rect.width;
+        height = rect.height;
+
+        const touch0 = event.touches[0];
+        const touch1 = event.touches[1];
+
+        distance = computeDistance(
+          touch0.pageX,
+          touch0.pageY,
+          touch1.pageX,
+          touch1.pageY
+        );
+      }
+
+      workspace.addEventListener(
+        "touchstart",
+        onContainerTouchstartListener
+      );
+      targetElement.addEventListener("touchmove", onTargetTouchmoveListener);
+
+      targetElement.addEventListener(
+        "touchend",
+        () => {
+          workspace.removeEventListener(
+            "touchstart",
+            onContainerTouchstartListener
+          );
+          targetElement.removeEventListener(
+            "touchmove",
+            onTargetTouchmoveListener
+          );
+
+          movableTargetElement = null;
+
+          console.log("touchend — target");
+        },
+        EVENT_LISTENER_OPTIONS
+      );
+    } else {
+      if (targetElement === selectedTargetElement) {
+        moveAt = targetMovement(touch, targetElement);
+      }
+
+      targetElement.addEventListener(
+        "touchend",
+        (event) => {
+          const endTime = new Date();
+          movableTargetElement = null;
+
+          if (endTime - startTime < 100) {
+            event.preventDefault();
+
+            const touch = event.changedTouches[0];
+            if (touch.target === selectedTargetElement) {
+              workspace.removeEventListener(
+                "touchstart",
+                onTargetTouchstartListener
+              );
+              workspace.removeEventListener(
+                "touchmove",
+                onTargetTouchmoveListener
+              );
+
+              selectedTargetElement.style.backgroundColor =
+                INITIAL_TARGET_COLOR;
+
+              selectedTargetElement = null;
+              isDoubleClickMode = false;
+
+              console.log("touchend — target");
+            }
+          }
+        },
+        EVENT_LISTENER_OPTIONS
+      );
+    }
+
+    isTouch = true;
+
+    console.log("touchstart — target");
+  });
+});
